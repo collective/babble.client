ac86bce8055a9f6fdaa7b0da0940f2b1bed0b72f
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-14 17:00:02 +0000

    remove the edit form for the portlet

--- slc/onlinecontacts/portlets/onlinecontacts.py
+++ slc/onlinecontacts/portlets/onlinecontacts.py
@@ -3,7 +3,7 @@ from zope.interface import implements
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
 
-from zope.formlib import form
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class IOnlineContacts(IPortletDataProvider):
@@ -14,92 +14,54 @@ class IOnlineContacts(IPortletDataProvider):
     same.
     """
 
-    # TODO: Add any zope.schema fields here to capture portlet configuration
-    # information. Alternatively, if there are no settings, leave this as an
-    # empty interface - see also notes around the add form and edit form
-    # below.
-
-    # some_field = schema.TextLine(title=_(u"Some field"),
-    #                              description=_(u"A field to use"),
-    #                              required=True)
-
-
 class Assignment(base.Assignment):
-    """Portlet assignment.
-
+    """ 
     This is what is actually managed through the portlets UI and associated
     with columns.
     """
 
     implements(IOnlineContacts)
 
-    # TODO: Set default values for the configurable parameters here
-
-    # some_field = u""
-
-    # TODO: Add keyword parameters for configurable parameters here
-    # def __init__(self, some_field=u""):
-    #    self.some_field = some_field
-
     def __init__(self):
         pass
 
     @property
     def title(self):
-        """This property is used to give the title of the portlet in the
-        "manage portlets" screen.
-        """
+        """ """
         return "Who's online?"
 
 
-
 class Renderer(base.Renderer):
-    """Portlet renderer.
-
+    """
     This is registered in configure.zcml. The referenced page template is
     rendered, and the implicit variable 'view' will refer to an instance
     of this class. Other methods can be added and referenced in the template.
     """
-
     render = ViewPageTemplateFile('onlinecontacts.pt')
+
+    def get_id(self):
+        """ """
+        return 'online-contacts-portlet-%d' % self.__hash__()
+
+    def get_authenticated_member(self):
+        """ """
+        pm = getToolByName(self, 'portal_membership')
+        return pm.getAuthenticatedMember()
     
     def get_online_contacts(self):
         """ """
-        return ['apilz', 'jcbrand', 'do3cc']
+        # XXX: Restrict this to online contacts only
+        member = self.get_authenticated_member()
+        pm = getToolByName(self, 'portal_membership')
+        members = pm.listMembers()
+        if member in members:
+            members.remove(member)
+        return members
 
 
-class AddForm(base.AddForm):
-    """Portlet add form.
-
-    This is registered in configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display. The create() method actually
-    constructs the assignment that is being added.
-    """
-    form_fields = form.Fields(IOnlineContacts)
-
-    def create(self, data):
-        return Assignment(**data)
+class AddForm(base.NullAddForm):
+    """ """
+    def create(self):
+        return Assignment()
 
 
-# NOTE: If this portlet does not have any configurable parameters, you
-# can use the next AddForm implementation instead of the previous.
-
-# class AddForm(base.NullAddForm):
-#     """Portlet add form.
-#     """
-#     def create(self):
-#         return Assignment()
-
-
-# NOTE: If this portlet does not have any configurable parameters, you
-# can remove the EditForm class definition and delete the editview
-# attribute from the <plone:portlet /> registration in configure.zcml
-
-
-class EditForm(base.EditForm):
-    """Portlet edit form.
-
-    This is registered with configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display.
-    """
-    form_fields = form.Fields(IOnlineContacts)
