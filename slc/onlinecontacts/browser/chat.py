da3ac866e99b51eac73407bd5a749b17c6b27a6e
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-14 16:58:09 +0000

    Implement the poll and send_message methods

--- slc/onlinecontacts/browser/chat.py
+++ slc/onlinecontacts/browser/chat.py
@@ -1,4 +1,5 @@
 import logging
+import xmlrpclib
 import simplejson as json
 from Products.CMFCore.utils import getToolByName
 
@@ -6,22 +7,42 @@ log = logging.getLogger('slc.onlinecontacts/browser/chat.py')
 
 class Chat:
 
+    def _getConnection(self):
+        """ Return a connection to the chat service """
+        mtool = getToolByName(self.context, 'portal_chat')
+        return mtool.getConnection()
+
     def _authenticated_member(self):
-        """ """
+        """ Return the currently logged in member object """
         pm = getToolByName(self, 'portal_membership')
         return pm.getAuthenticatedMember()
 
-    def send_chat(self, to, message):
+    def send_message(self, user, to, message):
         """ Send a chat message """
         log.info('Chat message %s sent to %s' % (message, to))
+        server = self._getConnection()
+        try:
+            server.sendMessage(user, to, message, True) # register=True
+        except xmlrpclib.Fault, e:
+            err_msg = e.faultString.strip('\n').split('\n')[-1]
+            log.error('Error from chat.service: send_message: %s' % err_msg)
+            raise err_msg 
+        return u''
 
-    def poll(self):
+    def poll(self, user):
         """ Polling to retrieve online users and chat messages """
-        log.info('poll called')
-        return json.dumps({'items': []})
+        server = self._getConnection()
+        try:
+            messages = server.getAllMessages(user, True) # register=True
+        except xmlrpclib.Fault, e:
+            err_msg = e.faultString.strip('\n').split('\n')[-1]
+            log.error('Error from chat.service: send_message: %s' % err_msg)
+            raise err_msg 
+
+        return json.dumps({'items': messages})
 
     def start_session(self):
-        """ Polling to retrieve online users and chat messages """
+        """ """
         log.info('start_session called')
         member = self._authenticated_member()
         return json.dumps({'username': member.id, 'items': []})
