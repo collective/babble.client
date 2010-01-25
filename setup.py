0849c5ddb30b8bfa95684bcc5dbb3367c91d7232
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-25 16:53:09 +0000

    Add tests_require with collective.js.blackbird

--- setup.py
+++ setup.py
@@ -3,31 +3,38 @@ import os
 
 version = '1.0'
 
-setup(name='babble.client',
-      version=version,
-      description="This product provides a portlet labeled 'Who's online?' which shows you a list of all online users and enables you to contact them.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
-      keywords='plone syslab.com portlet onlinecontacts',
-      author='Syslab.com, JC Brand',
-      author_email='brand@syslab.com',
-      url='http://plone.org',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['babble'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'simplejson',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+tests_require=[
+    'collective.js.blackbird',
+]
+
+setup(
+    name='babble.client',
+    version=version,
+    description="This product provides a portlet labeled 'Who's online?' which shows you a list of all online users and enables you to contact them.",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    ],
+    keywords='plone syslab.com portlet onlinecontacts',
+    author='Syslab.com, JC Brand',
+    author_email='brand@syslab.com',
+    url='http://plone.org',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['babble'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'simplejson',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
