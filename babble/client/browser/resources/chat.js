b9b2becfbfa3918fe200914d5c64eace845b8f71
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-25 16:56:00 +0000

    Cookie handling has been moved to it's own js file. Don't poll when no valid username is available.

--- babble/client/browser/resources/chat.js
+++ babble/client/browser/resources/chat.js
@@ -95,9 +95,11 @@ function initialize(){
                     }
                 }
             });
+            if (username) {
+                setTimeout('poll();', poll_time);
+            }
         }
     });
-    setTimeout('poll();', poll_time);
 }
 
 function reorderChats() {
@@ -224,6 +226,7 @@ function createChat(title, minimize) {
 }
 
 function poll(){
+    log.info('Poll: poll_time: ' + poll_time);
     if (window_focus == false) {
         var blink_number = 0;
         var title_changed = 0;
@@ -420,48 +423,4 @@ function toggleChat(title) {
 		jQuery('#chatbox_'+title+' .chat-input').css('display','none');
 	}
 }
-
-/* Copyright (c) 2006 Klaus Hartl (stilbuero.de) */
-jQuery.cookie = function(name, value, options) {
-    if (typeof value != 'undefined') { // name and value given, set cookie
-        options = options || {};
-        if (value === null) {
-            value = '';
-            options.expires = -1;
-        }
-        var expires = '';
-        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-            var date;
-            if (typeof options.expires == 'number') {
-                date = new Date();
-                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-            } else {
-                date = options.expires;
-            }
-            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
-        }
-        // CAUTION: Needed to parenthesize options.path and options.domain
-        // in the following expressions, otherwise they evaluate to undefined
-        // in the packed version for some reason...
-        var path = options.path ? '; path=' + (options.path) : '';
-        var domain = options.domain ? '; domain=' + (options.domain) : '';
-        var secure = options.secure ? '; secure' : '';
-        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-    } 
-    else { // only name given, get cookie
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-};
 
