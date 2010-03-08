df21f2b4f3cf77e68503235d181307169039b8a3
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-08 21:15:48 +0000

    Register users when initialized is called. Renamed the variable 'user' to 'username'.

--- babble/client/browser/chat.py
+++ babble/client/browser/chat.py
@@ -1,10 +1,14 @@
 import logging
+import random
 import xmlrpclib
 import simplejson as json
 from zope.interface import implements
 
+from AccessControl.unauthorized import Unauthorized
+
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
 
 from babble.client.browser.interfaces import IChat
 from babble.client.browser.interfaces import IChatBox
@@ -23,13 +27,16 @@ class Chat:
         return online_users
 
 
-    def initialize(self, user, open_chats):
+    def initialize(self, username, open_chats):
         """ Initializion by fetching all open chat sessions and their uncleared
             and unread chat messages
         """
-        log.info('initialize called, user: %s' % user)
+        log.info('initialize called, username: %s' % username)
+        if username == 'Anonymous User':
+            return
+
         open_chats = open_chats.split('|')
-        for s in ['', user]:
+        for s in ['', username]:
             if s in open_chats:
                 open_chats.remove(s)
 
@@ -37,9 +44,21 @@ class Chat:
         messages = []
         for chat_buddy in open_chats:
             try:
-                # username, sender, auto_register, read, clear, confirm_online
+                if not server.isRegistered(username):
+                    pm = getToolByName(self.context, 'portal_membership')
+                    member = pm.getAuthenticatedMember()
+                    if member.getId() != username:
+                        raise Unauthorized(
+                            '%s tried to authenticate as a different user %s' \
+                            % (member.getId(), username))
+
+                    password = str(random.random())
+                    member.manage_addProperty('chatpass', password, 'string')
+                    server.register(username, password)
+
+                # username, sender, read, clear, confirm_online
                 messages = server.getUnclearedMessages(
-                                    user, chat_buddy, True, True, False, True)
+                                    username, chat_buddy, True, False, True)
 
             except xmlrpclib.Fault, e:
                 err_msg = e.faultString
@@ -48,20 +67,20 @@ class Chat:
                 log.error('Error from chat.service: getUnclearedMessages: %s' % err_msg)
                 raise BabbleException(err_msg)
 
-        return json.dumps({'username': user, 'items': messages,})
+        return json.dumps({'username': username, 'items': messages,})
 
 
-    def poll(self, user):
+    def poll(self, username):
         """ Poll the chat server to retrieve new online users and chat
             messages
         """
-        if not user:
+        if not username:
             return
 
         server = utils.getConnection(self.context)
         try:
-            # pars: username, sender, register, read, confirm_online
-            messages = server.getUnreadMessages(user, None, True, True, True)
+            # pars: username, sender, read, confirm_online
+            messages = server.getUnreadMessages(username, None, True, True)
         except xmlrpclib.Fault, e:
             err_msg = e.faultString.strip('\n').split('\n')[-1]
             log.error('Error from chat.service: getUnreadMessages: %s' % err_msg)
@@ -70,27 +89,27 @@ class Chat:
         return json.dumps({'items': messages })
 
 
-    def send_message(self, user, to, message):
+    def send_message(self, username, to, message):
         """ Send a chat message """
         log.info('Chat message %s sent to %s' % (message, to))
         server = utils.getConnection(self.context)
         try:
-            server.sendMessage(user, to, message, True) # register=True
+            server.sendMessage(username, to, message, True) # register=True
         except xmlrpclib.Fault, e:
             err_msg = e.faultString.strip('\n').split('\n')[-1]
             log.error('Error from chat.service: sendMessage: %s' % err_msg)
             raise BabbleException(err_msg)
 
 
-    def get_last_conversation(self, user, chat_buddy):
+    def get_last_conversation(self, username, chat_buddy):
         """ Get all the uncleared messages between user and chat_buddy
         """
         log.info('get_last_conversation')
-        messages = utils.get_last_conversation(self.context, user, chat_buddy)
+        messages = utils.get_last_conversation(self.context, username, chat_buddy)
         return json.dumps({'messages': messages})
 
 
-    def clear_messages(self, user, chat_buddy):
+    def clear_messages(self, username, chat_buddy):
         """ Mark the messages in a chat contact's messagebox as cleared.
             This means that they won't be loaded and displayed again next time
             that chat box is opened.
@@ -98,8 +117,8 @@ class Chat:
         log.info('clear messages sent to buddy: %s' % (chat_buddy))
         server = utils.getConnection(self.context)
         try:
-            # passed pars: (username, sender, auto_register, read, clear)
-            server.getUnclearedMessages(user, chat_buddy, True, True, True)
+            # passed pars: (username, sender, read, clear)
+            server.getUnclearedMessages(username, chat_buddy, True, True, True)
         except xmlrpclib.Fault, e:
             err_msg = e.faultString.strip('\n').split('\n')[-1]
             log.error('Error from chat.service: clearMessages: %s' % err_msg)
@@ -111,9 +130,9 @@ class ChatBox(BrowserView):
     implements(IChatBox)
     template = ViewPageTemplateFile('templates/chatbox.pt')
 
-    def render_chat_box(self, box_id, user, title):
+    def render_chat_box(self, box_id, username, title):
         """ """
-        messages = utils.get_last_conversation(self.context, user, title)
+        messages = utils.get_last_conversation(self.context, username, title)
         return self.template(messages=messages, box_id=box_id, title=title)
 
 
