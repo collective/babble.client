e0606eb9ffab25fc5606850d6f920125aaefd2f8
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-07 16:25:52 +0000

    Consolidated the portlet tests. We now have 100% coverage for portlets

--- src/babble/client/tests/test_portlet.py
+++ src/babble/client/tests/test_portlet.py
@@ -17,6 +17,10 @@ class TestPortlet(TestCase):
         self.loginAsPortalOwner()
         view = self.app.unrestrictedTraverse('+/addChatService.html')
         view(add_input_name='chatservice', title='Chat Service', submit_add=1)
+
+        self.portal.portal_chat.manage_changeProperties(**{'use_local_service': True})
+
+        # The 'temp_folder' is not created for some reason, so do it here...
         self.app._setOb('temp_folder', Folder('temp_folder'))
 
     def test_portlet_type_registered(self):
@@ -31,6 +35,15 @@ class TestPortlet(TestCase):
         self.failUnless(IPortletAssignment.providedBy(portlet))
         self.failUnless(IPortletDataProvider.providedBy(portlet.data))
 
+    def test_add_form(self):
+        portlet = getUtility(IPortletType, name="babble.client.onlinecontacts")
+        mapping = self.portal.restrictedTraverse(
+                                    '++contextportlets++plone.leftcolumn'
+                                    )
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+        assignment = addview.create()
+        self.assertEquals(type(assignment), type(onlinecontacts.Assignment()))
+
     def test_obtain_renderer(self):
         context = self.folder
         request = self.folder.REQUEST
@@ -38,51 +51,38 @@ class TestPortlet(TestCase):
         manager = getUtility(IPortletManager, name='plone.rightcolumn',
                              context=self.portal)
 
-        # TODO: Pass any keyword arguments to the Assignment constructor
         assignment = onlinecontacts.Assignment()
 
         renderer = getMultiAdapter(
             (context, request, view, manager, assignment), IPortletRenderer)
         self.failUnless(isinstance(renderer, onlinecontacts.Renderer))
 
-
-class TestRenderer(TestCase):
-
-    def afterSetUp(self):
-        self.loginAsPortalOwner()
-        view = self.app.unrestrictedTraverse('+/addChatService.html')
-        view(add_input_name='chatservice', title='Chat Service', submit_add=1)
-
-        self.portal.portal_chat.manage_changeProperties(**{'use_local_service': True})
-
-        # The 'temp_folder' is not created for some reason, so do it here...
-        self.app._setOb('temp_folder', Folder('temp_folder'))
-
-    def renderer(self, context=None, request=None, view=None, manager=None,
-                 assignment=None):
-        context = context or self.folder
-        request = request or self.folder.REQUEST
-        view = view or self.folder.restrictedTraverse('@@plone')
-        manager = manager or getUtility(
-            IPortletManager, name='plone.rightcolumn', context=self.portal)
-
-        assignment = assignment or onlinecontacts.Assignment()
-        return getMultiAdapter((context, request, view, manager, assignment),
-                               IPortletRenderer)
-
     def test_render(self):
-        r = self.renderer(context=self.portal,
-                          assignment=onlinecontacts.Assignment())
-        r = r.__of__(self.folder)
+        """ """
+        context = self.portal
+        assignment = onlinecontacts.Assignment()
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(
+                IPortletManager, 
+                name='plone.rightcolumn', 
+                context=self.portal
+                )
+
+        renderer = getMultiAdapter(
+                            (context, request, view, manager, assignment),
+                            IPortletRenderer
+                            )
+
+        r = renderer.__of__(self.folder)
         r.update()
         output = r.render()
-        # TODO: Test output
+        self.assertEquals(type(output),unicode)
 
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestPortlet))
-    suite.addTest(makeSuite(TestRenderer))
     return suite
 
