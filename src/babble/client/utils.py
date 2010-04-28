eea2c1f1955f775d9b510812dea8cfe907cf2947
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-28 11:17:02 +0000

    Add urlize method to identify URLs

--- src/babble/client/utils.py
+++ src/babble/client/utils.py
@@ -1,3 +1,6 @@
+import re
+import urllib
+import string
 import logging
 import simplejson as json
 import xmlrpclib
@@ -6,10 +9,104 @@ from Products.CMFCore.utils import getToolByName
 
 log = logging.getLogger('babble.client/utils.py')
 
+# Configuration for urlize() function.
+LEADING_PUNCTUATION  = ['(', '<', '&lt;']
+TRAILING_PUNCTUATION = ['.', ',', ')', '>', '\n', '&gt;']
+punctuation_re = re.compile('^(?P<lead>(?:%s)*)(?P<middle>.*?)(?P<trail>(?:%s)*)$' % \
+    ('|'.join([re.escape(x) for x in LEADING_PUNCTUATION]),
+    '|'.join([re.escape(x) for x in TRAILING_PUNCTUATION])))
+simple_email_re = re.compile(r'^\S+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+$')
+del x # Temporary variable
+
+def get_string_words(text):
+    """ Break up a string into a list of words 
+    """
+    return re.compile(r'(\s+)').split(text)
+
+
+def reverse_escape(html):
+    """ Returns the given HTML with ampersands, quotes and angle brackets encoded.
+    """
+    return html.replace(
+                '&amp;',  '&').replace(
+                '&lt;',   '<').replace(
+                '&gt;',   '>').replace(
+                '&quot;', '"').replace(
+                '&#39;',  "'")
+
+def escape(html):
+    """ Returns the given HTML with ampersands, quotes and angle brackets encoded.
+    """
+    return html.replace(
+                '&', '&amp;').replace(
+                '<', '&lt;').replace(
+                '>', '&gt;').replace(
+                '"', '&quot;').replace(
+                "'", '&#39;')
+
+
+def trim_url(url, limit):
+    if limit and len(url) > limit:
+        return '%s...' % url[:max(0,limit-3)]
+
+    return url
+
+
+def urlize(text, url_limit=None, nofollow=False, blank=False, auto_escape=False):
+    """
+    Adapted from a similar method in Django
+
+    Works on http://, https://, www. links and links ending in .org, .net or
+    .com. Links can have trailing punctuation (periods, commas, close-parens)
+    and leading punctuation (opening parens) and it'll still do the right
+    thing.
+
+    url_limit:      the length to which URLs must be truncated
+    nofolllow:      whether the URLs will get a rel="nofollow" attribute.
+    blank:          whether the URLs will get a target="_blank"" attribute.
+    auto_escape:    whether URLs will get autoescaped.
+    """
+    words = get_string_words(text)
+    nofollow_attr = nofollow and ' rel="nofollow"' or ''
+    blank_attr = blank and ' target="_blank"' or ''
+    for i, word in enumerate(words):
+        match = None
+        if '.' in word or '@' in word or ':' in word:
+            match = punctuation_re.match(word)
+        if match:
+            lead, middle, trail = match.groups()
+            url = None
+            if middle.startswith('http://') or middle.startswith('https://'):
+                url = urllib.quote(middle, safe='/&=:;#?+*')
+
+            elif middle.startswith('www.') or ('@' not in middle and \
+                    middle and middle[0] in string.ascii_letters + string.digits and \
+                    (middle.endswith('.org') or middle.endswith('.net') or middle.endswith('.com'))):
+                url = urllib.quote('http://%s' % middle, safe='/&=:;#?+*')
+
+            elif '@' in middle and not ':' in middle and simple_email_re.match(middle):
+                url = 'mailto:%s' % middle
+                nofollow_attr = ''
+
+            # Make link.
+            if url:
+                trimmed = trim_url(middle, url_limit)
+                middle = '<a href="%s"%s%s>%s</a>' % (url, nofollow_attr, blank_attr, trimmed)
+                if auto_escape:
+                    middle = escape(middle)
+                words[i] = '%s%s%s' % (lead, middle, trail)
+            else:
+                if auto_escape:
+                    words[i] = escape(word)
+
+    return u''.join(words)
+
+
 def getConnection(context):
     """ Returns a connection to the chat service """
     mtool = getToolByName(context, 'portal_chat')
     return mtool.getConnection()
+
 
 def get_online_usernames(context):
     server = getConnection(context)
@@ -86,3 +183,4 @@ def get_last_conversation(context, contact):
     resp = json.loads(resp)
     return resp['messages']
 
+    	
