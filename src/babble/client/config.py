6cc9cc9296a3cfb688f9420b8bcbc3cfdd3b4159
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-24 20:37:37 +0000

    Add more constants. Dicts that are passed back

--- src/babble/client/config.py
+++ src/babble/client/config.py
@@ -1,12 +1,32 @@
 I18N_DOMAIN = 'babble.client'
-
 SUCCESS = 0
 AUTH_FAIL = -1
 TIMEOUT = 1
-SERVER_FAULT = 2
+SERVER_ERROR = SERVER_FAULT = 2
 NOT_FOUND = 3
 
 from datetime import datetime
 from pytz import utc
 NULL_DATE = datetime.min.replace(tzinfo=utc).isoformat()
+
+AUTH_FAIL_RESPONSE = {
+    'status': AUTH_FAIL, 
+    'last_msg_date': NULL_DATE, 
+    'chatroom_messages': {},
+    'messages': {}
+    }
+
+SERVER_ERROR_RESPONSE = {
+    'status': SERVER_ERROR, 
+    'last_msg_date': NULL_DATE, 
+    'chatroom_messages': {},
+    'messages': {}
+    }
+
+TIMEOUT_RESPONSE = {
+    'status': TIMEOUT, 
+    'last_msg_date': NULL_DATE, 
+    'chatroom_messages': {},
+    'messages': {}
+    }
 
