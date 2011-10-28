01297e6640bcb2d0d1388534fc4c9daa8a01de5c
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-28 19:44:05 +0000

    Add new event handler for when a new user has logged in for the first time.
    Get all chatrooms that this user has access to and tell the chat service to add
    him as a participant.

--- src/babble/client/events.py
+++ src/babble/client/events.py
@@ -1,6 +1,22 @@
-from zope.interface import implements
+import simplejson as json
+
+from zope.component import getMultiAdapter
 from zope.component.interfaces import ObjectEvent
+from zope.interface import Interface
+from zope.interface import implements
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+
+from zope.app.container.interfaces import IObjectRemovedEvent
+from zope.app.component.hooks import getSite
+
+from five import grok
+
+from Products.CMFCore.interfaces import IMemberData
+from Products.CMFCore.utils import getToolByName
+from Products.PlonePAS.interfaces.events import IUserInitialLoginInEvent
+
+from babble.client.utils import getConnection
+
 
 class ILocalRolesModifiedEvent(IObjectModifiedEvent):
     """The local roles on an event have been modified via the @@sharing page
@@ -9,3 +25,33 @@ class ILocalRolesModifiedEvent(IObjectModifiedEvent):
 class LocalRolesModifiedEvent(ObjectEvent):
     """ """
     implements(ILocalRolesModifiedEvent)
+
+
+@grok.subscribe(Interface, IUserInitialLoginInEvent)
+def registerUserForChatRooms(user, event):
+    """ """
+    site = getSite()
+    username = user.getId()
+
+    # Register the user for the chatservice
+    view = getMultiAdapter((site, site.REQUEST), name='babblechat')
+    view.initialize()
+    
+    mtool = getToolByName(site, 'portal_membership')
+    member = mtool.getMemberById(username)
+    password = getattr(member, 'chatpass')
+
+    catalog = getToolByName(site, 'portal_catalog')
+    chatrooms = catalog(portal_type='babble.client.chatroom',
+                        allowedRolesAndUsers=username ) 
+
+    s = getConnection(site)
+    for c in chatrooms:
+        chatroom = c.getObject()
+        path = '/'.join(chatroom.getPhysicalPath())
+        response = json.loads(s.addChatRoomParticipant(
+                                    username,
+                                    password,
+                                    path,
+                                    username ))
+
